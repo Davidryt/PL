87b761c229b2cdb317dc748f12d48a13f602f68b
alvaro-marco <alvaro.a65@hotmail.es>
2023-02-03 15:50:34 +0100

    feat: :sparkles: El código del año pasado

---
+++ Prácticas 2022/Práctica final/pruebas.c
@@ -0,0 +1,146 @@
+////////////////////////Prueba variable main
+int a = 3;
+int b;
+
+int z = 1, pepe, patata = 38, puerro=2;
+int z = 1, pepe, patata = 38;
+main (){
+    int c = 3;
+    int d;
+    int a = 1;
+    int b = 0;
+    c = 1+2;
+    d = 4;  
+    for(c=1; 1==1; c = c + 1){
+        int a=1;
+    }
+    //int max = (a>b) ? a : b;
+    puts("-----------------------------------------------------------------------------------------------------------------------------------");
+}
+
+////////////////////////Prueba funcion main
+func (int a, int b, int c = 1, int d){
+    int a = 3;
+    int b;
+    b = 4;
+    a = 1+2;
+    int z = 1, pepe, patata = 38;
+    printf(1+1);
+    printf("Cebolla",1+1,1+2);
+    puts("Hola mundo");
+     while (a == 2){
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    }
+    if(b>=2)
+    {
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    }
+}
+main (){
+    int a = 3;
+    int b;
+    b = 4;
+    a = 1+2; 
+    int z = 1, pepe, patata = 38; 
+    printf(1+1);
+    printf("Cebolla",1+1,1+2);
+    puts("Hola mundo");
+    while (a != 2){
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    }
+    if(b>=2)
+    {
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+        for(c=1; 1==1; c = c + 1){
+            int a=1;
+        }
+    }
+    puts("-----------------------------------------------------------------------------------------------------------------------------------");
+}
+
+////////////////////////Prueba completo
+int a = 3;
+int b;
+
+int c[5];
+
+int z = 1, pepe, patata = 38;
+func (int a, int b, int c=2){            // Error aquii
+    int a = 3;
+    int b;
+    b = 4;
+    a = 1+2;
+    int z = 1, pepe, patata = 38;
+    printf(1+1);
+    printf("%d",1+1,1+2);   
+    puts("Hola mundo"); 
+    while(b <= 2){
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    }
+    if(b>=2 || 1==1 && 1 != 2)
+    {
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    }
+    for(c=1; 1==1; c = c + 1){
+        int a=1;
+    }
+}
+main (){
+    int a = 3;
+    int b;
+    b = 4;
+    a = 1+2;
+    int z = 1, pepe, patata = 38;
+    printf(1+1);
+    printf("Cebolla",1+1,1+2);
+    puts("Hola mundo");
+    while (a == 2){
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    }
+    while (a>2){
+        puts("Hola mundo");
+    }
+    while (a<2){
+        puts("Hola mundo");
+    }
+    if(b>=2)
+    {
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+    } else{
+        int z = 1, pepe, patata = 38;
+        printf(1+1);
+        printf("Cebolla",1+1,1+2);
+        puts("Hola mundo");
+
+    }
+    for(c=1; 1==1; c = c + 1){
+        int a=1;
+    }
+    puts("-----------------------------------------------------------------------------------------------------------------------------------");
+}
+////////////////////////Cómo ejecutar
+// cat pruebas.c | ./a.out
---
+++ Prácticas 2022/Prácticas guiadas/calc1.y
@@ -0,0 +1,59 @@
+/*Grupo 3 - Álvaro Marco - Andrei Baltes */
+/*100383382@alumnos.uc3m.es 100406687@alumnos.uc3m.es*/
+
+%{                              /* Seccion 1  Declaraciones de C y BISON */
+#include <stdio.h>
+#define YYSTYPE  double
+%}
+                                /* Seccion 2  Declaraciones de BISON   */  
+%%
+                                /* Seccion 3  Sint - Semantico   */
+
+axioma:    axioma expresion '\n' { printf ("Expresion=%lf\n", $2);} 
+            | expresion '\n' {printf ("Expresion=%lf\n", $1);}
+           ;
+
+expresion:   operando                { $$ = $1; }
+           | operando '+' expresion  { $$ = $1 + $3; }
+           | operando '-' expresion  { $$ = $1 - $3; }
+           | operando '*' expresion  { $$ = $1 * $3; }
+           | operando '/' expresion  { $$ = $1 / $3; }
+           ;
+
+operando:    numero                  { $$ = $1; }
+           ;
+
+numero:      '0'                     { $$ = 0 ; }
+           | '1'                     { $$ = 1 ; }
+           | '2'                     { $$ = 2 ; }
+           | '3'                     { $$ = 3 ; }
+           | '4'                     { $$ = 4 ; }
+           | '5'                     { $$ = 5 ; }
+           | '6'                     { $$ = 6 ; }
+           | '7'                     { $$ = 7 ; }
+           | '8'                     { $$ = 8 ; }
+           | '9'                     { $$ = 9 ; }
+           ;
+
+%%
+                                        /* Seccion 4  Codigo en C   */
+int yyerror (char *mensaje)
+{
+    fprintf (stderr, "%s\n", mensaje) ;
+}
+
+int yylex ()
+{
+    unsigned char c ;
+
+    do {
+         c = getchar () ;
+    } while (c == ' ') ;
+
+    return c ;
+}
+
+int main ()
+{
+    yyparse () ;
+}
